dd6b7396e433f6fe7c2f5bceaf2bcb1ea2cd5c18
AllenKayy <allisonoyinkansola1@gmail.com>
2023-05-12 00:53:20 +0100

    Create a user object with request parameters and return as a json response

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,14 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.send('respond with a resource');
+});
+
+router.get("/:name/:age/:sex", (req, res) => {
+  const { name, age, sex } = req.params;
+  const user = { name, age, sex }
+  res.json(user);
 });
 
 module.exports = router;
